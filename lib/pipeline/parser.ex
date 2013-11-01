df1e7c8bd3b04effab59d72d962913c7eae4c054
colinbankier <colinbankier@gmail.com>
2013-11-01 23:20:51 +1000

    Convert Dicts from JSON into Models.

--- lib/pipeline/parser.ex
+++ lib/pipeline/parser.ex
@@ -1,15 +1,25 @@
 defmodule PipelineParser do
 
+  alias Models.Pipeline
+  alias Models.Task
+
   def read(filename) do
 
   end
 
   def parse(json) do
     {:ok, data} = JSEX.decode json
-    Enum.map data, &(convert(&1))
+    pipeline = convert data
+    {:ok, pipeline}
   end
 
   def convert elem do
-    Pipeline.new name: elem[:name]
+    cond do
+      Dict.has_key? elem, "tasks" ->
+        Pipeline.new name: elem["name"],
+         tasks: Enum.map(elem["tasks"], &(convert(&1)))
+      Dict.has_key? elem, "command" ->
+        Task.new name: elem["name"], command: elem["command"]
+    end
   end
 end
