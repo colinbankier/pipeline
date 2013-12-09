c9b36dafb66eba956cc9eaf9867f95d12b6dace2
colinbankier <colinbankier@gmail.com>
2013-12-09 17:13:19 +1000

    Clean up old pipeline runner code

--- lib/pipeline_runner.ex
+++ lib/pipeline_runner.ex
@@ -44,17 +44,6 @@ defmodule PipelineRunner do
     new_result |> update
   end
 
-  def _run(pipeline = Pipeline[], { pipeline_status, result_list }) do
-    {status, task_results} = Enum.reduce(pipeline.tasks, {:not_started, []}, &_run/2)
-    {status, [PipelineResult.new(id: 1, name: pipeline.name, status: status, tasks: Enum.reverse(task_results)) | result_list]}
-  end
-
-  def _run(task = Task[], { pipeline_status, result_list }) do
-    result = run_process(last_result(result_list), task.command)
-    task_result = TaskResult.new(name: task.name, output: result[:output], status: result[:status])
-    {overall_status(pipeline_status, result[:status]), [ task_result | result_list ] }
-  end
-
   def find_next_task [ head ], pipeline do
     nil
   end
@@ -122,17 +111,6 @@ defmodule PipelineRunner do
     find_or_init.(:ets.lookup(:pipeline_results, pipeline.name) |> Enum.first)
   end
 
-  def lookup_current_state(pipeline_result = PipelineResult[]) do
-    {status, task_results} = Enum.reduce(pipeline_result.tasks, &lookup_current_state/1)
-    {status, [PipelineResult.new(id: 1, name: pipeline_result.name, status: status, tasks: Enum.reverse(task_results)) ]}
-  end
-
-  def lookup_current_state(task_result = TaskResult[]) do
-    result = TaskRunner.lookup_task_output(task_result.pid)
-    task_result = TaskResult.new(name: task_result.name, output: result[:output], status: result[:status])
-    {overall_status(:none, result[:status]), [ task_result ] }
-  end
-
   def notify_task_complete pipeline, path, task_result do
     update_pipeline_result(current_state(pipeline), path, task_result) |> update
     if task_result.status == :ok do
@@ -154,47 +132,5 @@ defmodule PipelineRunner do
       task.name == head
     end
     List.replace_at(tasks, index, update_pipeline_result(Enum.at(tasks, index), path, task_result))
-  end
-
-  def overall_status(current_status, task_status) do
-    switch_status = fn
-      :error, :not_started -> :error
-      _, task_status -> task_status
-    end
-
-    switch_status.(current_status, task_status)
-  end
-
-  def last_result(result_list = [ last_result = head | tail ]) do
-    last_result.status
-  end
-
-  def last_result([]) do
-    :ok
-  end
-
-  def run_process(:ok, command) do
-    working_dir = PipelineApp.default_working_dir |> String.to_char_list!
-    pid = TaskRunner.run command, working_dir
-    [ output: "", status: :running, pid: pid ]
-  end
-
-  def run_process( _ , _) do
-    [ output: "", status: :not_started ]
-  end
-
-  def build_run_result({return_code, output}) do
-    [
-        output: single_string(Keyword.get(output, :stdout)) <> single_string(Keyword.get(output, :stderr)),
-        status: return_code
-    ]
-  end
-
-  def single_string(nil) do
-    ""
-  end
-
-  def single_string(output_list) do
-    Enum.reduce(output_list, "", &(&2 <> &1))
   end
 end
