b96096706d10d4b73d3ae05e2242a4e17b462808
colinbankier <colinbankier@gmail.com>
2014-05-29 17:13:42 +1000

    Add basic styles.

--- web/react/src/pipeline.js
+++ web/react/src/pipeline.js
@@ -18,15 +18,34 @@ var Task = React.createClass({
 });
 
 var Pipeline = React.createClass({
+  render: function() {
+    var taskNodes = this.props.tasks.map(function (task) {
+      if (task.type == "pipeline") {
+      return <Pipeline name={task.name} tasks={task.tasks} />;
+      } else {
+      return <Task name={task.name} />;
+      }
+    });
+    return (
+      <div className="pipeline">
+      <h2 className="pipelineName">
+       {this.props.name}
+       </h2>
+      <div className="taskList" class="task-list">
+        {taskNodes}
+      </div>
+      </div>
+    );
+  }
+});
+
+var PipelineView = React.createClass({
   loadFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
       success: function(data) {
         this.setState({data: data});
-        console.log("Hello");
-        console.log(data);
-        console.log(this.props);
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -40,122 +59,14 @@ var Pipeline = React.createClass({
     this.loadFromServer();
   },
   render: function() {
-    console.log(this.state.data);
-    var taskNodes = this.state.data.tasks.map(function (task) {
-      return <Task name={task.name} />;
-    });
     return (
-      <div className="pipeline">
-      <h2 className="pipelineName">
-       {this.state.data.name}
-       </h2>
-      <div className="taskList">
-        {taskNodes}
-      </div>
-      </div>
+      <Pipeline name={this.state.data.name} tasks={this.state.data.tasks} />
     );
   }
 });
 
-var Comment = React.createClass({
-render: function() {
-return (
-  <div className="comment">
-  <h2 className="commentAuthor">
-  {this.props.author}
-  </h2>
-  {this.props.children}
-  </div>
-  );
-}
-});
-var CommentList = React.createClass({
-render: function() {
-var commentNodes = this.props.data.map(function (comment) {
-  return <Comment author={comment.author}>{comment.text}</Comment>;
-  });
-return (
-  <div className="commentList">
-  {commentNodes}
-  </div>
-  );
-}
-});
-
-var CommentForm = React.createClass({
-  handleSubmit: function() {
-    var author = this.refs.author.getDOMNode().value.trim();
-    var text = this.refs.text.getDOMNode().value.trim();
-    this.props.onCommentSubmit({author: author, text: text});
-    if (!text || !author) {
-      return false;
-    }
-    // TODO: send request to the server
-    this.refs.author.getDOMNode().value = '';
-    this.refs.text.getDOMNode().value = '';
-    return false;
-  },
-  render: function() {
-    return (
-      <div className="commentForm">
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-      <input type="text" placeholder="Your name" ref="author" />
-      <input type="text" placeholder="Say something..." ref="text" />
-      <input type="submit" value="Post" />
-      </form>
-      </div>
-    );
-  }
-});
-var CommentBox = React.createClass({
-  loadCommentsFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleCommentSubmit: function(comment) {
-    var comments = this.state.data;
-    var newComments = comments.concat([comment]);
-    this.setState({data: newComments});
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: comment,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentWillMount: function() {
-    this.loadCommentsFromServer();
-    //setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-  },
-  render: function() {
-    return (
-      <div className="commentBox">
-      <h1>Comments</h1>
-      <CommentList data={this.state.data} />
-      <CommentForm onCommentSubmit={this.handleCommentSubmit} />
-      </div>
-    );
-  }
-});
 React.renderComponent(
-  <Pipeline url="pipeline.json"/>,
+  <PipelineView url="pipeline.json"/>,
   document.getElementById('content')
 );
 
