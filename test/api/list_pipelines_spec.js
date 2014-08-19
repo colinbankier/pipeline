fec087ebeb4a152097702442675b486e70ec60e2
colinbankier <colinbankier@gmail.com>
2014-08-20 07:44:11 +1000

    Add path to show spec - so can revert to if needed.

--- test/api/list_pipelines_spec.js
+++ test/api/list_pipelines_spec.js
@@ -24,10 +24,33 @@ frisby.create('List first level tasks of source repo Pipeline')
   .get(host + '/pipelines/Simple Pipeline')
   .expectJSON({
     name: "Simple Pipeline",
+    path: ["simple_pipeline"],
     tasks: [
-      { name: "task 1" },
-      { name: "task 2" },
-      { name: "task 3" }
+      { name: "task 1",
+        path: ["simple_pipeline", "Simple Pipeline"],
+        command: "echo 1"
+    },
+    { name: "task 2",
+      path: ["simple_pipeline", "Simple Pipeline"],
+      tasks: [
+        { name: "task 2a",
+          path: ["simple_pipeline", "Simple Pipeline", "task 2"],
+          command: "echo 2a"
+      },
+      { name: "task 2b",
+        path: ["simple_pipeline", "Simple Pipeline", "task 2"],
+        command: "echo 2b"
+      },
+      { name: "task 2c",
+        path: ["simple_pipeline", "Simple Pipeline", "task 2"],
+        command: "echo 2c"
+      }
+      ]
+    },
+    { name: "task 3",
+      path: ["simple_pipeline", "Simple Pipeline"],
+      command: "echo 3"
+    }
     ]
   })
   .toss();
