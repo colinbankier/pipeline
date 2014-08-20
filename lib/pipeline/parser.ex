413ceb7c035993279f7d3ba3f720d08110084c3c
colinbankier <colinbankier@gmail.com>
2014-08-20 17:45:10 +1000

    Lookup pipelines by source repo as top level key.

--- lib/pipeline/parser.ex
+++ lib/pipeline/parser.ex
@@ -5,38 +5,52 @@ defmodule Pipeline.Parser do
     SourceRepo.all |>
     pipeline_file_names |>
     read_pipeline_files |>
-    filter_out_read_errors
+    filter_out_read_errors |>
+    set_source_repo
   end
 
   def find_by_path path_string do
     Enum.find list_pipelines, fn(pipeline) ->
-      IO.puts "Comparing #{pipeline.name} and #{path_string}"
-      pipeline.name == path_string
+      pipeline.source_repo == path_string
     end
   end
 
   def pipeline_file_names source_repos do
     repo_dir = "test/resources"
     Enum.map source_repos, fn(repo) ->
-      repo_dir <> "/" <> repo.name <> "/" <> "pipeline.yml"
+      %{
+        source_repo: repo,
+        file_path: repo_dir <> "/" <> repo.name <> "/" <> "pipeline.yml"
+      }
     end
   end
 
   def read_pipeline_files config_files do
-    Enum.map config_files, fn(file_path) ->
-      Yamex.parse_yaml_file file_path
+    Enum.map config_files, fn(config_map) ->
+      Map.put config_map, :pipeline, parse_yaml_file(config_map.file_path)
     end
   end
 
+  def parse_yaml_file file_path do
+    Yamex.parse_yaml_file file_path
+  end
+
   def filter_out_read_errors pipelines do
-    Enum.filter pipelines, fn(pipeline) ->
+    Enum.filter pipelines, fn(config_map) ->
       try do
-        {:error, _} = pipeline
+        {:error, _} = config_map.pipeline
         false
       rescue
         e in MatchError ->
         true
       end
+    end
+  end
+
+  def set_source_repo config_maps do
+    Enum.map config_maps, fn(config_map) ->
+      config_map.pipeline |>
+      Map.put :source_repo, config_map.source_repo.name
     end
   end
 
--- test/api/list_pipelines_spec.js
+++ test/api/list_pipelines_spec.js
@@ -21,34 +21,29 @@ frisby.create('List first level tasks of source repo Pipeline')
   .toss();
 
   frisby.create("Show pipeline")
-  .get(host + '/pipelines/Simple Pipeline')
+  .get(host + '/pipelines/simple_pipeline')
+  .inspectJSON()
   .expectJSON({
     name: "Simple Pipeline",
-    path: ["simple_pipeline"],
+    source_repo: "simple_pipeline",
     tasks: [
       { name: "task 1",
-        path: ["simple_pipeline", "Simple Pipeline"],
         command: "echo 1"
     },
     { name: "task 2",
-      path: ["simple_pipeline", "Simple Pipeline"],
       tasks: [
         { name: "task 2a",
-          path: ["simple_pipeline", "Simple Pipeline", "task 2"],
           command: "echo 2a"
       },
       { name: "task 2b",
-        path: ["simple_pipeline", "Simple Pipeline", "task 2"],
         command: "echo 2b"
       },
       { name: "task 2c",
-        path: ["simple_pipeline", "Simple Pipeline", "task 2"],
         command: "echo 2c"
       }
       ]
     },
     { name: "task 3",
-      path: ["simple_pipeline", "Simple Pipeline"],
       command: "echo 3"
     }
     ]
