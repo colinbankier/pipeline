862ac06039cb8acc95461159fe5710f79ebcd65d
colinbankier <colinbankier@gmail.com>
2014-08-25 17:57:17 +1000

    Extend trigger job api test.

--- test/api/trigger_pipeline_spec.js
+++ test/api/trigger_pipeline_spec.js
@@ -9,11 +9,28 @@ frisby.create("Trigger a pipeline to run")
   .expectJSONTypes({
     id: Number
   })
+  .expectJSON({
+    status: "scheduled"
+  })
   .afterJSON(function(job) {
-    frisby.create("Get run result")
-    .get(host + '/jobs/' + job.id)
-    .expectStatus(200)
-    .inspectJSON()
-    .toss();
+    var status = "";
+    for (var i = 0; i < 10; i++) {
+      frisby.create("Get run result")
+      .get(host + '/jobs/' + job.id)
+      .expectStatus(200)
+      .afterJSON(function(result) {
+        status = result.status;
+      })
+      .inspectJSON()
+      .toss();
+    console.log(status);
+    if (status == "success") {
+      console.log("success");
+      break;
+    } else {
+      console.log(i);
+    }
+    }
+    expect(status).toEqual("success");
   })
   .toss();
