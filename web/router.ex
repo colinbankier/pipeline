ba549024a367ed5f2d9aacbbe18ada9ddd790280
colinbankier <colinbankier@gmail.com>
2016-05-17 15:52:33 +1000

    Copy static resources from trello, for now.

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule PipelineApp.Router do
   scope "/", PipelineApp do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ var config = module.exports = {
   },
 
   resolve: {
-    extesions: ['', '.js', '.sass'],
+    extensions: ['', '.js', '.sass'],
     modulesDirectories: ['node_modules'],
   },
 
